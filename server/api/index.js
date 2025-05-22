748b7bf52d203e1f358a53ff481912b14bb82adb
anarb0 <anarb359@gmail.com>
2025-05-21 21:17:54 -0400

    deployment

--- server/api/index.js
+++ server/api/index.js
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
-import userRouter from "./routers/userRoutes.js";
-import courseRouter from "./routers/courseRoutes.js";
-import sectionRouter from "./routers/sectionRoutes.js";
-import videoRouter from "./routers/videoRoutes.js";
-import paymentRouter from "./routers/paymentRoutes.js";
-import enrollmentRouter from "./routers/enrollmentRoutes.js";
+import userRouter from "../routers/userRoutes.js";
+import courseRouter from "../routers/courseRoutes.js";
+import sectionRouter from "../routers/sectionRoutes.js";
+import videoRouter from "../routers/videoRoutes.js";
+import paymentRouter from "../routers/paymentRoutes.js";
+import enrollmentRouter from "../routers/enrollmentRoutes.js";
 
 const app = express();
 const PORT = 3001;
@@ -17,7 +17,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://suraafrontend.vercel.app",
     credentials: true,
   })
 );
@@ -31,6 +32,7 @@ app.use("/api/videos", videoRouter);
 app.use("/api/payments", paymentRouter);
 app.use("/api/enrollments", enrollmentRouter);
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.listen(PORT, () => {
   console.log(`Server running on:${PORT}`);
 });
