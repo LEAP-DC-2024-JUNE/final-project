c0d109adac288a997bc45b59641f42e3aaabca75
anarb0 <anarb359@gmail.com>
2025-06-01 14:44:43 -0400

    implement GET courses by instructor method

--- client/src/components/Instructor/Courses/CourseCard.tsx
+++ client/src/components/Instructor/Courses/CourseCard.tsx
@@ -1,9 +1,14 @@
 "use client";
-import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
 
+import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
+import { Course } from "@/utils/types";
 import { motion } from "framer-motion";
 
-const CourseCard = () => {
+interface CourseCardProps {
+  course: Course;
+}
+
+const CourseCard = ({ course }: CourseCardProps) => {
   return (
     <motion.div
       whileHover={{ scale: 1.05 }}
@@ -12,9 +17,18 @@ const CourseCard = () => {
     >
       <Card className=" w-[300px] h-[300px]">
         <div className=" flex flex-col justify-between items-center">
-          <CardTitle>Tittle</CardTitle>
-          <CardContent>Image</CardContent>
-          <CardFooter>Description</CardFooter>
+          <CardTitle>{course.title}</CardTitle>
+          <CardContent>
+            <img
+              src={course.imageUrl}
+              alt={course.title}
+              className="w-full h-32 object-cover rounded"
+            />
+          </CardContent>
+          <CardFooter className="flex flex-col">
+            <div>{course.description}</div>
+            <div>${course.price}</div>
+          </CardFooter>
         </div>
       </Card>
     </motion.div>
--- client/src/components/Instructor/Courses/MyCourses.tsx
+++ client/src/components/Instructor/Courses/MyCourses.tsx
@@ -1,8 +1,44 @@
 "use client";
+import { useEffect, useState } from "react";
 import CourseCard from "./CourseCard";
 import { motion } from "framer-motion";
+import { useAuth } from "@clerk/nextjs";
+import { Course } from "@/utils/types";
 
 const MyCourses = () => {
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const token = await getToken({ template: "suraa" });
+
+        if (!token) {
+          throw new Error("No token found. Please sign in again.");
+        }
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/courses/instructor/me`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const data = await res.json();
+        setCourses(data);
+      } catch (error) {
+        console.error("Failed to fetch courses:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCourses();
+  }, []);
   return (
     <div className=" flex flex-col items-start justify-between mx-10">
       <motion.div
@@ -11,12 +47,19 @@ const MyCourses = () => {
         transition={{ duration: 0.5 }}
       >
         <p className="font-medium">Courses I own:</p>
-        <div className=" py-10 flex flex-row items-center gap-10 flex-wrap">
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
-        </div>
+        {loading ? (
+          <p className="mt-10">Loading courses...</p>
+        ) : (
+          <div className="py-10 flex flex-row items-center gap-10 flex-wrap">
+            {courses.length > 0 ? (
+              courses.map((course, index) => (
+                <CourseCard key={index} course={course} />
+              ))
+            ) : (
+              <p>No courses found.</p>
+            )}
+          </div>
+        )}
       </motion.div>
     </div>
   );
